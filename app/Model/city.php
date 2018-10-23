d09f38f3725e46bda02a3c8f1d952f480cce2a6f
Tôn Thất Nhật Quân <nhatquan160697@gmail.com>
2018-10-23 10:12:02 +0700

    Update city.php

--- app/Model/city.php
+++ app/Model/city.php
@@ -9,6 +9,10 @@ class city extends Model
     //
     protected $table="city";
     public $timestamps = false;
+    protected $primaryKey = "id";
+    public function getItems(){
+    	return $this->all();
+    }
     public function users_properties()
     {
     	return $this->hasMany('App\user_properties','City','id');
