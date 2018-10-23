6402d48b8f0c202568460a3efd3805202f99de2b
Ton Quan <nhatquan160697@gmail.com>
2018-10-23 10:52:39 +0700

    Update file city

--- app/Model/city.php
+++ app/Model/city.php
@@ -10,9 +10,11 @@ class city extends Model
     protected $table="city";
     public $timestamps = false;
     protected $primaryKey = "id";
+
     public function getItems(){
     	return $this->all();
     }
+
     public function users_properties()
     {
     	return $this->hasMany('App\user_properties','City','id');
