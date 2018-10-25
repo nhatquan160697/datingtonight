0dd4076b2b661bbec2fcb9773162b32273d958be
Ton Quan <nhatquan160697@gmail.com>
2018-10-25 14:02:37 +0700

    Update interface login user and admin

---
+++ resources/views/auth/users/formLogin.blade.php
@@ -0,0 +1,11 @@
+<form action="#" method="post">
+	<div class="form-left-w3l">
+		<input type="text" class="top-up" name="name" placeholder="Name" required="">
+	</div>
+	<div class="form-right-w3ls ">
+		<input type="password" name="password" placeholder="Password" required="">
+	</div>
+	<div class="btnn">
+		<button type="submit">LOGIN</button><br>
+	</div>
+</form>
